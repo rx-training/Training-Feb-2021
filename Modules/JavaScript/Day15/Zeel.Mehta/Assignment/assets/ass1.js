cce8db679b4cf450d861bc8fa65e3dead758e0ad
zeel mehta <zeel92000@gmail.com>
2021-02-24 17:45:26 +0530

    commit

--- Modules/JavaScript/Day15/Zeel.Mehta/Assignment/assets/ass1.js
+++ Modules/JavaScript/Day15/Zeel.Mehta/Assignment/assets/ass1.js
@@ -1,5 +1,35 @@
 function cal(num1,num2)
 {
-    num3=parseInt(num1)+parseInt(num2);
-    console.log(num3);
+    var s = document.getElementsByName('sum'); 
+    var op;
+    for(i = 0; i < s.length; i++) { 
+        if(s[i].checked) 
+            op= s[i].value;
+    }
+   
+    switch(op){
+        case 0:
+            num3=num1+num2;
+            
+            break;
+        case 1:
+            num3=parseInt(num1)-parseInt(num2);
+            
+            break;
+        case 2:
+            num3=parseInt(num1)*parseInt(num2);
+            
+            break;
+        case 3:
+            num3=parseInt(num1)/parseInt(num2);
+            
+            break;
+        default:
+            num3=0;
+            
+            break;
+
+    }
+    document.getElementById("result").innerHTML= num3;
+    
 }
