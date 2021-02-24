f58a1ab2e0faa9c43230e708603eac83da584317
Tarun <taroonm345@gmail.com>
2021-02-24 09:57:29 +0530

    changes

--- Modules/Bootstrap4/Day14/tarun moradiya/final project/src/templates/javascript/day16/assignment/Date Validation/js/script.js
+++ Modules/Bootstrap4/Day14/tarun moradiya/final project/src/templates/javascript/day16/assignment/Date Validation/js/script.js
@@ -18,39 +18,55 @@ myform.onsubmit = function(event) {
 
     if (!Number.isNaN(mon)) {
         if (mon < 1 || mon > 12) {
+            output.classList.remove('text-success');
+            output.classList.add('text-danger');
             output.innerHTML = '<br>invalid! month should be between 1 - 12';
             valid = false;
         }
     } else {
+        output.classList.remove('text-success');
+        output.classList.add('text-danger');
         output.innerHTML = '<br>invalid! input should be digits only';   
         valid = false;
     }   
     
     if (!Number.isNaN(dat)) {
         if (dat < 0 || dat > 31) {
+            output.classList.remove('text-success');
+            output.classList.add('text-danger');
             output.innerHTML = '<br>invalid! day should be between 1 - 31';
             valid = false;
         }
     } else {
+        output.classList.remove('text-success');
+        output.classList.add('text-danger');
         output.innerHTML = '<br>invalid! input should be digits only';   
         valid = false;
     }
     
     if (!Number.isNaN(year)) {
         if (! /[0-9]{4}/.test(year)) {
+            output.classList.remove('text-success');
+            output.classList.add('text-danger');
             output.innerHTML = '<br>invalid! year should be four digits';
             valid = false;
         } else if (year == 0) {
+            output.classList.remove('text-success');
+            output.classList.add('text-danger');
             output.innerHTML = '<br>invalid! year should be greater then 0';
             valid = false;
         } 
     } else {
+        output.classList.remove('text-success');
+        output.classList.add('text-danger');
         output.innerHTML = '<br>invalid! input should be digits only';   
         valid = false;
     }
 
     if (valid == true) {
-        output.innerHTML = '<br><h1>Date is Valid</h1>';   
+        output.classList.remove('text-danger');
         output.classList.add('text-success');
+        output.innerHTML = '<br><h1>Date is Valid</h1>';   
+        
     }
 }
