9ac4903751f2e879387f00c5f8de476c237c5d0a
tarun patidar <patidartarun355@gmail.com>
2021-03-04 18:47:23 +0530

    REFS: #123 and fixes #123( SQL Day 1)

---
+++ Modules/SQL/Day1/tarunpatidar/practice/SQLQuery5.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Employees (
+
+	EmployeeID int PRIMARY KEY IDENTITY,
+	FirstName varchar(50) NOT NULL,
+	LastName varchar(50) NOT NULL,
+	Email varchar(50) NOT NULL,
+	PhoneNumber varchar(10) NOT NULL,
+	JoiningDate date NOT NULL,
+	JobID int NOT NULL,
+	Salary varchar(50) NOT NULL,
+	ManagerID int NOT NULL,
+	DepartmentID int NOT NULL
+	
+	
+);
+
+INSERT INTO Employees VALUES ('Tarun','Patidar','patidartarun355@gmail.com','9630627424','01-02-2021',1,2500000,1,1);
