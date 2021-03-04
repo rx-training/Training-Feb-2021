08e8a87e19cdde46618e5c74f524cb64e5e17d3d
kartikraval <kartikraval2295@yahoo.com>
2021-03-04 13:22:16 +0530

    Day1 SQL Update

--- Modules/SQL/Day1/kartik.raval/Practice/JobHistory.sql
+++ Modules/SQL/Day1/kartik.raval/Practice/JobHistory.sql
@@ -1,18 +1,15 @@
 USe RadixTraining
+
 CREATE TABLE JobHistorys
 (
 EmployeeID INT NOT NULL,
 StartDate Date NOT NULL,
-EndDate Varchar(20) CONSTRAINT chkEndDate CHECK(EndDate like '^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$'),
+EndDate Varchar(20) CONSTRAINT chkEndDate CHECK(EndDate like '[0-3][0-9]/[0-1][0-9]/[1-2][0-9][0-9][0-9]'),
 JobID INT NOT NULL,
 DepartmentID INT NOT NULL,
 UNIQUE (EmployeeID, JobID, DepartmentID)
 )
 
-select convert(varchar, getdate(), 103) 
-
 SELECT * FROM JobHistorys
 
 DROP TABLE JobHistorys
-
-delete * from JobHistorys where EmployeeID = 3;
