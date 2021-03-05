d973925c6ccb14463e24baa8d3f238f8ea1ea5c8
SampatHarshit <sampatharshit03@gmail.com>
2021-03-05 16:31:01 +0400

    REFS:#123FIXES#123 Commit alter practice

---
+++ Modules/SQL/Day_1/HarshitSampat/Day_1_Alter_Practice.sql
@@ -0,0 +1,7 @@
+Use Demo1;
+
+ALTER TABLE JobHistories ADD CONSTRAINT FK_JOB_ID FOREIGN KEY (JOBID) REFERENCES Jobs(JObID);
+
+ALTER TABLE Jobhistories DROP CONSTRAINT FK_JOB_ID;
+
+ALTER TABLE Jobhistories ADD location varchar(10);
