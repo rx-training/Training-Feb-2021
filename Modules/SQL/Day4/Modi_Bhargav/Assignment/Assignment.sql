287d472c4f9c103985b0cc3d8505f5bb55460ff6
ModiBhargav98 <modibhargav1998@gmail.com>
2021-03-10 15:01:45 +0530

    Changes Done

--- Modules/SQL/Day4/Modi_Bhargav/Assignment/Assignment.sql
+++ Modules/SQL/Day4/Modi_Bhargav/Assignment/Assignment.sql
@@ -4,7 +4,7 @@ SELECT * FROM Employees
 SELECT FirstName, LastName,Salary,RANK() OVER (ORDER BY Salary) AS Rank FROM Employees
 
 /***** 2.Select 4th Highest salary from employee table using ranking function. *****/
-SELECT * FROM (SELECT EmployeeID,FirstName, LastName,Salary , DENSE_RANK() OVER (ORDER BY Salary DESC) '4Th Highest' FROM Employees) AS D WHERE '4Th Highest' = 4;
+SELECT * FROM (SELECT EmployeeID,FirstName, LastName,Salary , DENSE_RANK() OVER (ORDER BY Salary DESC) 'Highest' FROM Employees) AS D WHERE Highest = 4;
 
 /***** 3.Get department, total salary with respect to a department from employee table.*****/
 SELECT DepartmentID, SUM(Salary) AS 'ID To Salary' FROM Employees GROUP BY DepartmentID
@@ -16,7 +16,7 @@ SELECT DepartmentID, SUM(Salary) AS 'ID To Salary' FROM Employees GROUP BY Depar
 SELECT DepartmentID, Max(Salary) AS 'MaxSalary' FROM Employees GROUP BY DepartmentID
 
 /***** 6.Get department wise minimum salary from employee table order by salary ascending.*****/
-SELECT DepartmentID, Max(Salary) AS 'MinSalary' FROM Employees GROUP BY DepartmentID
+SELECT DepartmentID, Min(Salary) AS 'MinSalary' FROM Employees GROUP BY DepartmentID
 
 /***** 7.Select department, total salary with respect to a department from employee table where total salary greater than 50000 order by TotalSalary descending.*****/
 SELECT DepartmentID, SUM(Salary) AS 'ID To Salary' FROM Employees GROUP BY DepartmentID HAVING SUM(Salary) > 50000 ORDER BY 'ID To Salary' DESC;
