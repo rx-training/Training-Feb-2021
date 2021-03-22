23547fbc47ba04d86ecd36e83a44ab7cd109c50e
Tarun <taroonm345@gmail.com>
2021-03-22 13:07:33 +0530

    day14 complete

---
+++ Modules/SQL/Day14/tarun moradiya/assignment/assignmentTable.sql
@@ -0,0 +1,28 @@
+CREATE SCHEMA Day14
+GO
+
+CREATE TABLE Day14.Students(
+StudentID int CONSTRAINT Students_StudentID_PK PRIMARY KEY,
+StudentName varchar(50),
+TotalFees money,
+RemainingAmt money
+)
+
+CREATE TABLE Day14.Courses (
+CourseID int CONSTRAINT Courses_CourseID_PK PRIMARY KEY,
+CourseName varchar(50),
+TotalFees money
+)
+
+CREATE TABLE Day14.CourseEnrolled(
+StudentID int CONSTRAINT CourseEnrolled_StudentID_FK FOREIGN KEY REFERENCES Day14.Students(StudentID),
+CourseID int CONSTRAINT CourseEnrolled_CourseID_FK FOREIGN KEY REFERENCES Day14.Courses(CourseID)
+)
+
+CREATE TABLE Day14.FeePayment (
+StudentID int CONSTRAINT FeePayment_StudentID_FK FOREIGN KEY REFERENCES Day14.Students(StudentID),
+AmountPaid money,
+DateofPayment date
+)
+
+
