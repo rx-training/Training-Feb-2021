caa27ce77428f902b382648aa75c9e9d75d29b11
Tarun <taroonm345@gmail.com>
2021-04-06 12:16:41 +0530

    changes

--- Modules/NodeJS/Day2/Tarun Moradiya/Practice Exercise/exercise/readEx.js
+++ Modules/NodeJS/Day2/Tarun Moradiya/Practice Exercise/exercise/readEx.js
@@ -22,25 +22,58 @@ try {
     console.error(err)
 }
 
+//async and await
+
+async function getData(fileName, interval) {
+    
+     var returnData = await fs.readFile(path.join(__dirname, fileName), 'utf8', (err, data) => {
+        if (err) {
+           console.error(err)
+           return
+        } else {
+            setTimeout(() => console.log(data) , interval)
+          
+        }
+    })
+    return returnData
+}
+
+async function showData() {
+    try {
+       await getData('textFile1.txt', 2000)
+
+       await getData('textFile2.txt', 3000)
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+showData()
+
+
+
 //reading files
 
 //async
-fs.readFile(path.join(__dirname, 'textFile1.txt'), 'utf8', (err, data) => {
-    if (err) {
-        console.error(err)
-        return
-    } else {
-        console.log(data)
-    }
-})
+// fs.readFile(path.join(__dirname, 'textFile1.txt'), 'utf8', (err, data) => {
+//     if (err) {
+//         console.error(err)
+//         return
+//     } else {
+//         console.log(data)
+//     }
+// })
 
-//sync
+// //sync
 
-try{
-    let data = fs.readFileSync(path.join(__dirname, 'textFile2.txt'), 'utf8')
-    console.log(data)
-} catch(err) {
-    console.error(err)
-}
+// try{
+//     let data = fs.readFileSync(path.join(__dirname, 'textFile2.txt'), 'utf8')
+//     console.log(data)
+// } catch(err) {
+//     console.error(err)
+// }
+
+
 
 
