a3d01f427f87005ab3cf8db15241943b6b154b1e
Jay Prajapati <jayprajapati7085@gmail.com>
2021-04-09 10:36:59 +0530

    REFS #201 and fixes #201 C# Day2 Task Completed

---
+++ Modules/C#/Day2/Jay Prajapati/Practice/ClassesAndObject/ClassesAndObject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassesAndObject
+{
+    class Employee
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Emp1 = new Employee()
+            {
+                Age = 35,
+                Name = "Jay",
+                Salary = 120000.67,
+                DateOfJoining = new DateTime(2020,05,21),
+                PhoneNumber = "1234567890"
+
+            }
+        }
+    }
+}
