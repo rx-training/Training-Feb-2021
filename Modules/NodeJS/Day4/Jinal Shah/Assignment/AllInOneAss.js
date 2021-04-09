17282a3b4d5a4438045ce39b9f0416e021c4d7a5
jinalshah <jinalkshah1@gmail.com>
2021-04-09 12:25:13 +0530

    changes in day 4 (REFS: #202 and fixes: #202)

--- Modules/NodeJS/Day4/Jinal Shah/Assignment/AllInOneAss.js
+++ Modules/NodeJS/Day4/Jinal Shah/Assignment/AllInOneAss.js
@@ -1,7 +1,58 @@
 /* Write a Nodejs server that listen on port 3001 and which will read person.json and return a response in 
    JSON format. */
 
+   const fs = require('fs');
+   const url = require('url');
    const http = require('http');
+
+   const server = http.createServer((req,res) => {
+       var path = new URL('http://localhost:3001' + req.url)
+
+       if(path.pathname === '/'){
+            fs.readFile("./person.json",'utf8',(err,data) => {
+                if(err) console.log(err)
+                else if(data){
+                   res.write(data.toString())
+                   res.end()
+                }
+            });
+       }
+
+       else if (path.pathname === '/product'){
+           const n1 = parseInt(path.searchParams.get('param1'));
+           const n2 = parseInt(path.searchParams.get('param2'));
+
+           res.write(`Sum of ${n1} and ${n2} is = ${n1 + n2}`);
+           res.end()
+       }
+
+       else if(path.pathname == '/vowefind'){
+           const str = path.searchParams.get('input');
+           const upstr = str.toUpperCase();
+
+           for(i =0; i <= upstr.length; i++){
+               if(upstr[i] == 'A' || upstr[i] == 'E' || upstr[i] == 'I' || upstr[i] == 'O' || upstr[i] == 'U')
+               {
+                res.write(`First vowel character in string '${str}' is : ${upstr[i]}`)
+                res.end()
+                break;
+               }
+           }
+       }
+
+       else if(path.pathname == '/upload'){
+            fs.readFile('./html.html',(err,data) =>{
+                res.write(data)
+                res.end()
+            })
+       }
+
+   }).listen(3001)
+
+    
+
+
+   /* const http = require('http');
    var fs = require('fs');
 
    const url = require('url');
@@ -57,4 +108,4 @@
        })
    });
 
-   server.listen(3001);
+   server.listen(3001); */
