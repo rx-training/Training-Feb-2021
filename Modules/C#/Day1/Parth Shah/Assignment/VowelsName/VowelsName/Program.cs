7faea9fdf60742c8a95c8fcfc6cbc1cf7e1693ce
parthshah889900 <parthshah889900@gmail.com>
2021-04-09 10:30:05 +0530

    Day 1 C# (REFS #201 Completed assignment and task , constants , enum )

---
+++ Modules/C#/Day1/Parth Shah/Assignment/VowelsName/VowelsName/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace VowelsName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myStr;
+            int i, len, vowel_count;
+
+            myStr = "Parth Shah";
+
+            vowel_count = 0;
+     
+
+            // find length
+            len = myStr.Length;
+
+            for (i = 0; i < len; i++)
+            {
+                if (myStr[i] == 'a' || myStr[i] == 'e' || myStr[i] == 'i' || myStr[i] == 'o' || myStr[i] == 'u' || myStr[i] == 'A'
+                   || myStr[i] == 'E' || myStr[i] == 'I' || myStr[i] == 'O' || myStr[i] == 'U')
+                {
+                    vowel_count++;
+                }
+               
+            }
+
+            Console.Write("\nVowels in the string: {0}\n", vowel_count);
+        }
+    }
+}
