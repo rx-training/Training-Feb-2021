1fe70a3be53f4891ca2143ca2b5acd941df4e46c
Tarun <taroonm345@gmail.com>
2021-04-13 01:30:42 +0530

    day 7 complete REFS #202 and fixes #202

---
+++ Modules/NodeJS/Day7/Tarun Moradiya/PracticeExercise/videoPractice/asyn-demo/1_syncVsasync.js
@@ -0,0 +1,7 @@
+console.log('Before')
+
+setTimeout(() => {
+    console.log('Reading user from database...')
+}, 2000);
+
+console.log('after')
---
+++ Modules/NodeJS/Day7/Tarun Moradiya/PracticeExercise/videoPractice/asyn-demo/4_callbackHell.js
@@ -0,0 +1,36 @@
+console.log('Before')
+
+getUser(1, function(user) {
+
+    //get repositories
+    getRepositories(user.gitHubUserName, (repos) => {
+        getCommits(repo, (commits) => {
+            //CALLBACK HELL / CRISTMAS TREE PROBLEM
+
+        })
+    })
+})
+
+console.log('after')
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log('Reading user from database...')
+        callback({id: id, gitHubUserName: 'Tarun'})
+    }, 2000);
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log('calling an api..')
+        callback(['repo1', 'repo2', 'repo3'])
+    }, 2000);
+}
+
+//syncronous version looks cleaner
+
+console.log('Before')
+const user = getUser(1)
+const repos = getRepositories(user.gitHubUserName)
+const commits = getCommits(repos[0])
+console.log('After')
