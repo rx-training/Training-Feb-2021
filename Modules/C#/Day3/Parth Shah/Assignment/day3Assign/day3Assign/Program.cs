785caea4edf84abd50a556b0d41ec5529e5f3ac9
parthshah889900 <parthshah889900@gmail.com>
2021-04-12 13:49:35 +0530

    day 3 C# (REFS#201and Fixes 201)

---
+++ Modules/C#/Day3/Parth Shah/Assignment/day3Assign/day3Assign/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace day3Assign
+{    
+        interface I1
+        {
+            void Get();
+
+            void Display();
+
+            int Salary();
+
+        }
+
+        public abstract class Employee : I1
+        {
+            int ID;
+            string name, Address, pannumber;
+
+            public void Get()
+            {
+                Console.WriteLine("Enter ID:");
+                ID = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Name: ");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter address: ");
+                Address = Console.ReadLine();
+                Console.WriteLine("Enter Pan number: ");
+                pannumber = Console.ReadLine();
+            }
+            public virtual void Display()
+            {
+                Console.WriteLine($"\nEmployee ID is: {ID} ,\nEmployee Name is : {name} ,\n Employee Address is : {Address} ,\nEmployee Pannumber is : {pannumber} ");
+            }
+            public abstract int Salary();
+        }
+        public class PartTime : Employee
+        {
+            int SalaryPerHour, NoOfHour, salary;
+
+            public  void Get()
+            {
+                base.Get();
+                Console.WriteLine("Enter Salary PerHour :");
+                SalaryPerHour = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter No of Hour :");
+                NoOfHour = Convert.ToInt32(Console.ReadLine());
+                
+            }
+            public override void Display()
+            {
+                base.Display();
+                Console.WriteLine($"\nSalary Per Hour is : {SalaryPerHour},\nTotal No of Hours {NoOfHour},\nTotal Salary is : {salary}");
+
+            }
+            public override int Salary()
+            {
+                salary = NoOfHour * SalaryPerHour;
+                return salary;
+            }        
+        }
+
+        public class FullTime : Employee
+        {
+            int Basic, HRA, DA, TA, salary;
+            public void Get()
+            {
+                base.Get();
+                Console.WriteLine("Enter Basic Salary of Employee:");
+                Basic = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Basic HRA of Employee:");
+                HRA = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter DA of Employee:");
+                DA = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter TA of Employee:");
+                TA = Convert.ToInt32(Console.ReadLine());
+               
+            }
+
+            public override int Salary()
+            {
+                salary = Basic + DA + HRA + TA;
+                return salary;
+            }
+
+            public override void Display()
+            {
+                base.Display();
+                Console.WriteLine($"\nBasic is :{Basic},\nDA is : {DA} ,\n HRA is : {HRA},\nTA is : {TA} ,\nsalary is :{salary}" );
+            }
+        
+        class Program { 
+        static void Main(string[] args)
+        {
+            PartTime p = new PartTime();
+            p.Get();
+            p.Salary();
+            p.Display();
+
+            FullTime f = new FullTime();
+            f.Get();
+            f.Salary();
+            f.Display();
+        }
+    }
+    }
+}
---
+++ Modules/C#/Day3/Parth Shah/Practice/day3Practice/day3Practice/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace day3Practice
+{
+    class Vehicle  // base class (parent) 
+    {
+        public string brand = "Ford";  // Vehicle field
+        public void honk()             // Vehicle method 
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+
+    class Car : Vehicle  // derived class (child)
+    {
+        public string modelName = "Mustang";  // Car field
+    }
+    public class Employee
+    {
+        public float salary = 40000;
+    }
+    public class Programmer : Employee
+    {
+        public float bonus = 10000;
+    }
+
+    //polymorphism example :
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer p1 = new Programmer();
+
+            Console.WriteLine("Salary: " + p1.salary);
+            Console.WriteLine("Bonus: " + p1.bonus);
+
+            // Create a myCar object
+            Car myCar = new Car();
+
+            // Call the honk() method (From the Vehicle class) on the myCar object
+            myCar.honk();
+
+            // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
+            Console.WriteLine(myCar.brand + " " + myCar.modelName);
+
+
+
+            Animal myAnimal = new Animal();  // Create a Animal object
+            Animal myPig = new Pig();  // Create a Pig object
+            Animal myDog = new Dog();  // Create a Dog object
+
+            myAnimal.animalSound();
+            myPig.animalSound();
+            myDog.animalSound();
+
+
+           
+        }
+    }
+}
