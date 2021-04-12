17ba72f7d86eeea5ebad869b7ac2692438c55a1a
ModiBhargav98 <modibhargav1998@gmail.com>
2021-04-12 11:05:11 +0530

    (Day-2) Changes Done

--- Modules/NodeJS/Day2/Modi_Bhargav/Practices/Practices.js
+++ Modules/NodeJS/Day2/Modi_Bhargav/Practices/Practices.js
@@ -46,7 +46,7 @@ async function readFiles(folder,folder1) {
       }
       console.log(data)
     })
-    await fs.readFileSync(folder1,'utf8',(err,data) => {
+    await fs.readFile(folder1,'utf8',(err,data) => {
       if (err) {
         console.error(err)
         return
