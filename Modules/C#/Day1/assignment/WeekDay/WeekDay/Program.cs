fe0d9cf77a1851bcf2233c32889aeb7417e62ac8
tarun patidar <patidartarun355@gmail.com>
2021-04-14 20:17:30 +0530

    REFS: #201 and fixes #201 (C# Day1 to 6)

---
+++ Modules/C#/Day1/assignment/WeekDay/WeekDay/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WeekDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DayOfWeek today = DateTime.Today.DayOfWeek;
+            Console.WriteLine("Today is {0}", today);
+
+            if (today == DayOfWeek.Monday)
+            {
+                Console.WriteLine("DO WORK");
+            }
+
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}",
+                DayOfWeek.Monday,
+                DayOfWeek.Tuesday,
+                DayOfWeek.Wednesday,
+                DayOfWeek.Thursday,
+                DayOfWeek.Friday,
+                DayOfWeek.Saturday,
+                DayOfWeek.Sunday);
+        }
+
+    }
+}
---
+++ Modules/C#/Day6/assignment/AssignmentDay6/AssignmentDay6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AssignmentDay6
+{
+    class Program
+    {
+        public delegate Area Func<in x, in y, out Area>(int x, int y);
+
+        public static int AreaMethod(int x, int y)
+        {
+            return x * y;
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.Write("Enter First Number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Width : ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Func<int, int, int> area = AreaMethod;
+            Console.WriteLine("Area of rectangle : " + area(a, b));
+        }
+    }
+}
