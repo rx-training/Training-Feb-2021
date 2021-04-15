e7ea130e08b6d81f300de7f685375f279d2fe3e6
parthshah889900 <parthshah889900@gmail.com>
2021-04-15 14:26:43 +0530

    day 6 practice c# (REFS #201 and fixes #201)

---
+++ Modules/C#/Day6/Parth Shah/Practice/day6practice/day6practice/logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace day6practice
+{
+    class logger
+    {
+        public void Subscribe(clock theclock)
+        {
+            theclock.TimeChanged += new clock.TimeChangeHandler(NewTime);
+        }
+        public void NewTime(object theclock, TimeEventArgs e)
+        {
+            Console.WriteLine(" logging event at {0}:{1}:{2}",
+                e.Hour.ToString(),
+                e.Minute.ToString(),
+                e.Second.ToString());
+        }
+    }
+}
