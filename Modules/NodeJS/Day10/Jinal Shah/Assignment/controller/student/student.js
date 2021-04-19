6e4d081a4f1727d944ff7293e6deec9c83c41f01
jinalshah <jinalkshah1@gmail.com>
2021-04-19 12:19:28 +0530

    REFS: #202 and fixes: #202

---
+++ Modules/NodeJS/Day10/Jinal Shah/Assignment/controller/student/student.js
@@ -0,0 +1,62 @@
+var express = require('express')
+var router = express.Router();
+
+const result = require('./result/result')
+const fees = require('./fees/fees')
+
+const student = require('../../mongoDB/stud')
+
+class A{
+
+    static async list(req,res){
+        const sdata =await student.find()
+        res.send(sdata)
+    }
+
+    static async find(req,res){
+        const ID1 = parseInt(req.params.id)
+        const selData = await student.find({ ID : ID1 })
+        if(selData.length == 0) res.status(404).send("record not found..")
+        res.send(selData)
+    }
+
+    static async insert(req,res){
+        console.log('inserting..')
+        const newdata = req.body
+        const data1 = new student(newdata)
+        const result = await data1.save()
+        res.send(result)
+    }
+
+    static async update(req,res){
+        const ID1 = parseInt(req.params.id)
+        const selData = await student.find({ ID : ID1 })
+        if(selData.length == 0) res.status(404).send("record not found..")
+        const newdata = req.body
+        for(let i in newdata){
+            selData[0][i] = newdata[i]
+        }
+        const result = await selData[0].save()
+        res.send(result)
+    }
+
+    static async delete(req,res){
+        const ID1 = parseInt(req.params.id)
+        const selData = await student.find({ ID : ID1 })
+        if(selData.length == 0) res.status(404).send("record not found..")
+        const remove = await student.remove({ ID : ID1 })
+        res.send(selData)
+    }
+
+}
+
+router.get('/', A.list)
+router.get('/:id',A.find)
+router.post('/',A.insert)
+router.put('/:id',A.update)
+router.delete('/:id',A.delete)
+
+router.use('/:id/child/result',result)
+router.use('/:id/child/fees',fees)
+
+module.exports = router
