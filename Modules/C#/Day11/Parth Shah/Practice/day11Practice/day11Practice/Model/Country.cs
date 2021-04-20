8dc57c5b96d6305fc76b327005f966c039e07560
parthshah889900 <parthshah889900@gmail.com>
2021-04-20 14:43:17 +0530

    Day11 c# entitiy framework core(REFS #201 and Fixes #201)

---
+++ Modules/C#/Day11/Parth Shah/Practice/day11Practice/day11Practice/Model/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace day11Practice.Model
+{
+    public partial class Country
+    {
+        public string CountryId { get; set; }
+        public string CountryName { get; set; }
+        public decimal? RegionId { get; set; }
+    }
+}
