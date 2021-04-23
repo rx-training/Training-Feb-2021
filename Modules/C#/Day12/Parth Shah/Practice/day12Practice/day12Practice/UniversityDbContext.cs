c3c7c08bb307c045286c38c6c72b05185d1ede0e
parthshah889900 <parthshah889900@gmail.com>
2021-04-23 13:55:31 +0530

    day12C# EFCore (REFS #201 and Fixes #201) Completed Pratice task

---
+++ Modules/C#/Day12/Parth Shah/Practice/day12Practice/day12Practice/UniversityDbContext.cs
@@ -0,0 +1,23 @@
+﻿using day12Practice.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day12Practice
+{
+    class UniversityDbContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<studentAddress> StudentAddresses { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-VBU96E5\SQLEXPRESS01;Initial Catalog=Day12Pratice;Integrated Security=True");
+
+        }
+    }
+}
