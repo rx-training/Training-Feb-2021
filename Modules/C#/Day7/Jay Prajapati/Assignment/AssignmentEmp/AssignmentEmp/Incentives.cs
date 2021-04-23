73847696fc8401587f3455d6be274b93c42284ed
Jay Prajapati <jayprajapati7085@gmail.com>
2021-04-23 13:45:49 +0530

    REFS #201 and fixes #201 C# Day7 task

---
+++ Modules/C#/Day7/Jay Prajapati/Assignment/AssignmentEmp/AssignmentEmp/Incentives.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentEmp
+{
+    class Incentives
+    {
+        public int ID { get; set; }
+        public double IncentiveAmount { get; set; }
+        public DateTime IncentiveDate { get; set; }
+
+    }
+}
