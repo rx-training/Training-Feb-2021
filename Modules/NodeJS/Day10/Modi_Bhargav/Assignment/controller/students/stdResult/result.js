8274f566d63811fae5b751d60b10d16773fb2c7d
ModiBhargav98 <modibhargav1998@gmail.com>
2021-04-23 10:01:04 +0530

    REFS:#202 and Fixes:#202 (Day-10) Task_Done

---
+++ Modules/NodeJS/Day10/Modi_Bhargav/Assignment/controller/students/stdResult/result.js
@@ -0,0 +1,32 @@
+const student = require('../../../Models/stu_model')
+const express = require("express");
+const resultRouter = express.Router({ mergeParams: true });
+
+class Result {
+  static async resultFind(req, res) {
+    const ID = parseInt(req.params.id)
+    const resultFind = await student.find({ stdId: ID }).select('Result')
+
+    if (resultFind.length == 0) res.status(404).send("Your Id Is Not Found")
+
+    res.send(resultFind[0])
+  }
+  // static async resultUpdate(req, res) {
+
+  //   const ID = parseInt(req.params.id)
+  //   const resultUpdate = await student.find({ stdId: ID }).select('Result')
+
+  //   if (resultUpdate.length == 0) res.status(404).send("Your Id Is Not Found")
+
+  //   resultUpdate[0].Result = req.body;
+  //   const result = await resultUpdate[0].save()
+  //   res.send(result);
+  // };
+};
+
+
+resultRouter.get('/', Result.resultFind)
+// resultRouter.put('/', Result.resultUpdate)
+
+
+module.exports = resultRouter
---
+++ Modules/NodeJS/Day10/Modi_Bhargav/Assignment/controller/students/student.js
@@ -0,0 +1,65 @@
+const student = require('../../Models/stu_model')
+const express = require("express");
+const router2 = express.Router();
+const stdFees = require('./stdFees/fees')
+const stdResult = require('./stdResult/result')
+
+class Students {
+  static async StudentList(req, res) {
+    const stuData = await student.find()
+    res.send(stuData);
+  }
+  static async StudentFind(req, res) {
+
+    // specify stuId to Find data
+    const ID = parseInt(req.params.id)
+    const selectData = await student.find({ stdId: ID })
+
+    // unique object id to find data
+    // const selectData = await student.findById(req.params.id) 
+
+    if (selectData.length == 0) res.status(404).send("Your Id Is Not Found")
+    res.status(200).send(selectData);
+  }
+
+  // static async InsertData(req, res) {
+  //   const addData = req.body
+  //   const newData = new student(addData)
+  //   const result = await newData.save()
+  //   res.send(result)
+  // };
+  // static async UpdateData(req, res) {
+
+  //   const ID = parseInt(req.params.id)
+  //   const dataUpdate = await student.find({ stdId: ID })
+  //   if (dataUpdate.length == 0) res.status(404).send("Your Id Is Not Found")
+  //   const newData = req.body
+  //   for (let i in newData) {
+  //     dataUpdate[0][i] = newData[i]
+  //   }
+  //   const result = await dataUpdate[0].save()
+  //   res.send(result)
+  // }
+  // static async DeleteData(req, res) {
+  //   const ID = parseInt(req.params.id)
+
+  //   const dataDelete = await student.find({ stdId: ID })
+
+  //   if (dataDelete.length == 0) res.status(404).send("Your Id Is Not Found")
+
+  //   const remove = await student.remove({ stdId: ID })
+
+  //   res.send(dataDelete)
+  // }
+}
+
+router2.get('/all', Students.StudentList);
+router2.get('/:id', Students.StudentFind);
+// router2.post('/all', Students.InsertData);
+// router2.put('/:id', Students.UpdateData);
+// router2.delete('/:id', Students.DeleteData);
+
+router2.use('/:id/child/fees', stdFees);
+router2.use('/:id/child/result', stdResult);
+
+module.exports = router2;
