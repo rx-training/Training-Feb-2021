d0605ae2f6e1a9bdfb29c04f43bb82625698b47c
Jay Prajapati <jayprajapati7085@gmail.com>
2021-04-23 14:01:19 +0530

    changes

---
+++ Modules/C#/Day9/Jay Prajapati/Practice/ExtensionMethods/ExtensionMethods/IntExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    public static class IntExtensions
+    {
+        public static bool IsGreaterThan(this int i, int value)
+        {
+            return i > value;
+        }
+    }
+}
