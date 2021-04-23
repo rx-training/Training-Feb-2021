6f0892cf0a51a303f6e8c546c11c069777bdccb1
Jay Prajapati <jayprajapati7085@gmail.com>
2021-04-23 13:51:23 +0530

    REFS #201 and Fixes #201 Day 12/13 task

---
+++ Modules/C#/Day12/Jay Prajapati/Assignment/ToyManufacturingCompany/ToyManufacturingCompany/Data/ToyManufacturingCompanyContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using ToyManufacturingCompany.Models;
+
+namespace ToyManufacturingCompany.Data
+{
+    public class ToyManufacturingCompanyContext : DbContext
+    {
+        public DbSet<Toy> Toys { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerAddress> CustomerAddresses { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ProductOrder> ProductOrders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=ToyShopDB;Trusted_Connection=True;");
+        }
+    }
+}
