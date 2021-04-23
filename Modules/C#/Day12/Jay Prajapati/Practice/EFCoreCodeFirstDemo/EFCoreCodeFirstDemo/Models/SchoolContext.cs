5cb3dfa527044c5258f1dcd386ff914f7cbba015
Jay Prajapati <jayprajapati7085@gmail.com>
2021-04-23 13:53:21 +0530

    REFS #201 and fixes #201 Day 12/13 task

---
+++ Modules/C#/Day12/Jay Prajapati/Practice/EFCoreCodeFirstDemo/EFCoreCodeFirstDemo/Models/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreCodeFirstDemo.Models
+{
+    public class SchoolContext : DbContext
+    {
+ 
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+
+        }
+
+    }
+}
---
+++ Modules/C#/Day12/Jay Prajapati/Practice/EFCoreCodeFirstDemo/EFCoreCodeFirstDemo/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreCodeFirstDemo.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public object Grade { get; internal set; }
+        public object StudentCourses { get; internal set; }
+    }
+}
