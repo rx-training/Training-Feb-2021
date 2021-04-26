fd84187b637895ae9665bb87a89b9219d7cc6514
anujpal82 <anujpal631998@gmail.com>
2021-04-26 17:11:17 -0500

    commit to local

--- Modules/NodeJS/Day13/AnujPal/Assignment/Router/route.js
+++ Modules/NodeJS/Day13/AnujPal/Assignment/Router/route.js
@@ -1,52 +1,15 @@
-const mongoose = require('mongoose');
 const express = require('express')
 const router = express.Router();
-
-const { Router, response } = require('express');
-router.use(express());
-var bodyParser = require('body-parser');
-const { create } = require('../Models/doctor');
-var jsonParser = bodyParser.json()
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-const assignment = require("../Domain/class");
-
-router.get("/", assignment.start);
-
-router.post("/HA", jsonParser, assignment.postHA)
-
-router.post("/insertDepartment", jsonParser, assignment.insertDepartment)
-
-router.post("/insertPatient", jsonParser, assignment.insertPatient)
-
-router.post("/insertDoctor", jsonParser, assignment.inserDoctor)
-
-router.get("/getAllDoctors", jsonParser, assignment.getAllDoctors)
-
-router.get("/getDoctors/:did", jsonParser, assignment.doctorByDid)
-
-router.put("/updateDoctor/:did", jsonParser, assignment.updateDoctor);
-
-router.delete("/deleteDoctor/:did", assignment.deleteDoctor);
-
-router.get("/getAllPatients/", jsonParser, assignment.getAllPatients)
-
-router.get("/getPatient/:pid", jsonParser, assignment.getPatientById)
+const doctorRouter = require("../Router/Doctor/doctor")
+const patientRouter = require("../Router/Patient/patient")
+const departmentRouter = require("../Router/Department/department");
+const healthassistant = require("../Router/HealthAssistant/healthAssistant")
 
 
-// FInd all medicines of the patient
-
-router.get("/getPatient/medicines/:pid", jsonParser, assignment.getMedicines)
-
-
-// For knowing the status of patient
-router.get("/getStatus/:did", jsonParser ,assignment.patientStatus)
+router.use("/Doctor", doctorRouter)
+router.use("/Patient", patientRouter)
+router.use("/Department", departmentRouter)
+router.use("/HA", healthassistant)
 
 
-// Display summary report of Doctor and patient
-
-router.get("/summary/:did", jsonParser,assignment.summary)
-
-
-module.exports = router;
-
+module.exports = router
