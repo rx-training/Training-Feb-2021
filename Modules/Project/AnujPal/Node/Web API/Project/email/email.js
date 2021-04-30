435c67f89972d691a57a7f5476f2e45fb8bbcf19
anujpal82 <anujpal631998@gmail.com>
2021-04-30 10:51:53 -0500

    REFS: #202 and fixes #202

---
+++ Modules/Project/AnujPal/Node/Web API/Project/email/email.js
@@ -0,0 +1,42 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'anujpal631998@gmail.com',
+        pass: 'anujpal631998.'
+    }
+});
+
+var mailOptionsSignUp = {
+    from: 'anujpal631998@gmail.com',
+    to: 'anujpal160180107030@gmail.com',
+    subject: 'SignUp',
+    text: `You Signup the system!!!!!!!!`
+           
+};
+
+var mailOptionsLogin = {
+    from: 'anujpal631998@gmail.com',
+    to: 'anujpal160180107030@gmail.com',
+    subject: 'Login',
+    html:"<h3>You login to the system !!!!!!!!!</h3>"
+           
+};
+
+var mailOptionsCredit = {
+    from: 'anujpal631998@gmail.com',
+    to: 'anujpal160180107030@gmail.com',
+    subject: 'Credit',
+    html:"</h3>You Credited Some Amount </h3>"
+           
+};
+var mailOptionsDebit = {
+    from: 'anujpal631998@gmail.com',
+    to: 'anujpal160180107030@gmail.com',
+    subject: 'Debit',
+    html:"</h3>You Debited Some Amount </h3>"
+           
+};
+
+module.exports={transporter,mailOptionsLogin,mailOptionsSignUp,mailOptionsCredit,mailOptionsDebit}
