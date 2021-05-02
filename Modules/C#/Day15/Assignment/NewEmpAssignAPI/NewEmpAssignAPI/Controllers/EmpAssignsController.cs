8d25181df3d514a010d8343f8635a25cfc86f46f
Jay Prajapati <jayprajapati7085@gmail.com>
2021-05-02 10:12:55 +0530

    REFS #201 and fixes #201

---
+++ Modules/C#/Day15/Assignment/NewEmpAssignAPI/NewEmpAssignAPI/Controllers/EmpAssignsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewEmpAssignAPI.Models;
+
+namespace NewEmpAssignAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpAssignsController : ControllerBase
+    {
+        private readonly EmpAssignDBContext _context;
+
+        public EmpAssignsController(EmpAssignDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/EmpAssigns
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EmpAssign>>> GetEmpAssigns()
+        {
+            return await _context.EmpAssigns.ToListAsync();
+        }
+
+        // GET: api/EmpAssigns/5
+        [HttpGet("{id}")]
+        
+        public async Task<ActionResult<EmpAssign>> GetEmpAssign(int id)
+        {
+            var empAssign = await _context.EmpAssigns.FindAsync(id);
+
+            if (empAssign == null)
+            {
+                return NotFound();
+            }
+
+            return empAssign;
+        }
+        [HttpGet]
+        [Route("/{empId}/Assignments")]
+        public ActionResult<List<EmpAssign>> GetAllAssignments(int empId)
+        {
+            var Assignments = _context.EmpAssigns.Where(s => s.EmpId == empId).ToList();
+            return Assignments;
+        }
+
+        [HttpGet]
+        [Route("/{empId}/Assignments/{AsignId}")]
+        public ActionResult<List<EmpAssign>> GetAllAssignmentsWithID(int empId,int AsignId)
+        {
+            var Assignments = _context.EmpAssigns.Where(s => s.EmpId == empId && s.AssignemntId == AsignId).ToList();
+            return Assignments;
+        }
+
+        // PUT: api/EmpAssigns/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmpAssign(int id, EmpAssign empAssign)
+        {
+            if (id != empAssign.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(empAssign).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmpAssignExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/EmpAssigns
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<EmpAssign>> PostEmpAssign(EmpAssign empAssign)
+        {
+            _context.EmpAssigns.Add(empAssign);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmpAssign", new { id = empAssign.Id }, empAssign);
+        }
+
+        // DELETE: api/EmpAssigns/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmpAssign(int id)
+        {
+            var empAssign = await _context.EmpAssigns.FindAsync(id);
+            if (empAssign == null)
+            {
+                return NotFound();
+            }
+
+            _context.EmpAssigns.Remove(empAssign);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmpAssignExists(int id)
+        {
+            return _context.EmpAssigns.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Modules/C#/Day15/Assignment/NewEmpAssignAPI/NewEmpAssignAPI/Models/Assignment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NewEmpAssignAPI.Models
+{
+    public partial class Assignment
+    {
+        public Assignment()
+        {
+            EmpAssigns = new HashSet<EmpAssign>();
+        }
+
+        public int AssignmentId { get; set; }
+        public string AssignmentName { get; set; }
+        public string AssignmentNumber { get; set; }
+        public DateTime EffectiveStartDate { get; set; }
+        public DateTime EffectiveEndDate { get; set; }
+
+        public virtual ICollection<EmpAssign> EmpAssigns { get; set; }
+    }
+}
