889b2ff0b68e5e2fbe050461a321071d88935c40
parthshah889900 <parthshah889900@gmail.com>
2021-05-04 17:32:24 +0530

    day 17 Practice and Assignment (REFS :#201 and fixes #201)

---
+++ Modules/C#/Day17/Parth Shah/Assignment/day17Assignment/day17Assignment/Models/Department.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+    using day17assignment.Model;
+
+#nullable disable
+
+namespace day17assignment.Model
+{
+    public partial class Department
+    {
+        public Department()
+        {
+            Doctors = new HashSet<Doctor>();
+            HealthCareAssistants = new HashSet<HealthCareAssistant>();
+            Patients = new HashSet<Patient>();
+        }
+
+        public int DeptId { get; set; }
+        public string DeptName { get; set; }
+
+        public virtual ICollection<Doctor> Doctors { get; set; }
+        public virtual ICollection<HealthCareAssistant> HealthCareAssistants { get; set; }
+        public virtual ICollection<Patient> Patients { get; set; }
+    }
+}
