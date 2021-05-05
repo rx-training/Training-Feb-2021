bec898ba1aa5052725679b31b67ee04dfc98b552
parthshah889900 <parthshah889900@gmail.com>
2021-05-05 13:11:45 +0530

    REFS: #246 and fixes: #246(Day2 Practice Typescript)

---
+++ Modules/TypeScript/Day2/Parth Shah/Practice/function.js
@@ -0,0 +1,10 @@
+//named function
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 5));
+///anomyous function 
+var sum = function (a, y) {
+    return a + y;
+};
+console.log(sum(5, 5));
