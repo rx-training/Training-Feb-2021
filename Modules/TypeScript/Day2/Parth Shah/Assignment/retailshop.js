1fb50d0461b4167c0a1b2ac24f3032a50d5543b6
parthshah889900 <parthshah889900@gmail.com>
2021-05-06 23:16:56 +0530

    REFS: #246 and fixes: #246(Day 2 Assignment)

---
+++ Modules/TypeScript/Day2/Parth Shah/Assignment/retailshop.js
@@ -0,0 +1,51 @@
+//Assignment of day 2 ::
+console.log('==============================Welcome to PARTH GENERAL STORE ============================================\n');
+//store details of invntories in array
+var inv = [{ InventoryId: 1, InventoryName: 'newspaper', InventoryQuantity: 15, InventoryPrice: 1500, InvDescription: 'Only Gujarat samachar' },
+    { InventoryId: 2, InventoryName: 'books', InventoryQuantity: 30, InventoryPrice: 2500, InvDescription: 'Only NCERT' },
+    { InventoryId: 3, InventoryName: 'toys', InventoryQuantity: 10, InventoryPrice: 1600 }];
+//create class
+var RetailStocks = /** @class */ (function () {
+    function RetailStocks() {
+    }
+    //make function for place order
+    RetailStocks.prototype.Place = function (id, qty) {
+        var order = inv.filter(function (i) { return i.InventoryId == id; });
+        if (qty > 4) {
+            if (order[0] == null) {
+                console.log('==================================');
+                console.log('Return Valid Id ');
+            }
+            else if (order[0].InventoryQuantity - qty >= 0) {
+                inv[inv.indexOf(order[0])].InventoryQuantity = inv[inv.indexOf(order[0])].InventoryQuantity - qty;
+                console.log('==================================');
+                console.log('congrats! ur order placed!');
+            }
+            else {
+                console.log('==================================');
+                console.log('Sorry bro not in stock!  ');
+            }
+        }
+        else {
+            console.log('==================================');
+            console.log('\nQty should be greater than 5');
+        }
+    };
+    //make a function for availaible stock :
+    RetailStocks.prototype.StockAvailaible = function () {
+        console.log('Lists of stocks availaible :');
+        for (var _i = 0, inv_1 = inv; _i < inv_1.length; _i++) {
+            var val = inv_1[_i];
+            console.log("Id is : " + val.InventoryId + ", Name is : " + val.InventoryName + " , Quantiy is : " + val.InventoryQuantity + ", Price of inventory is  :" + val.InventoryPrice + " Inventory Description " + val.InvDescription);
+        }
+    };
+    return RetailStocks;
+}());
+var n = new RetailStocks();
+n.StockAvailaible();
+n.Place(2, 5);
+n.StockAvailaible();
+n.Place(1, 10);
+n.StockAvailaible();
+n.Place(1, 10);
+n.StockAvailaible();
