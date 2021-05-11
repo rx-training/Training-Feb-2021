559767ad06c0e8943df871970528e298a0d58b0d
jinalshah <jinalkshah1@gmail.com>
2021-05-11 10:16:24 +0530

    REFS : #244 and fixes #244

--- Modules/React/Day7/Jinal Shah/city-tours/src/tourData.js
+++ Modules/React/Day7/Jinal Shah/city-tours/src/tourData.js
@@ -4,27 +4,27 @@ export const tourData = [
   city : 'new york',
   img : 'newyork.jpg',
   name : 'new york bridge tour',
-  info : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, temporibus?"
+  info : "New York City's crossings date back to 1693, when its first bridge, known as the King's Bridge, was constructed over Spuyten Duyvil Creek between Manhattan and the Bronx, located in the present-day Kingsbridge neighborhood. The bridge, composed of stone abutments and a timber deck, was demolished in 1917. The oldest crossing still standing is High Bridge, built 1848 to carry the Croton Aqueduct from Manhattan to the Bronx over the Harlem River.This bridge was built to carry water to the city as part of the Croton Aqueduct system."
  },
  {
   id : 2,
   city : 'paris',
   img : 'paris.jpg',
   name : 'paris lights tour',
-  info : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, temporibus?"
+  info : "From the late 18th century to the 19th century, the city of Paris became increasingly known as a centre of education and ideas throughout the whole of Europe, inspiring poets, philosophers, engineers and scientists galore. As well as the gradual increase in wattage, this context of innovation and enlightenment is what helped reinforce the symbolic significance of Paris as ‘The City of Light’."
  },
  {
   id : 3,
   city : 'london',
   img : 'london.jpg',
   name : 'london royal palace tour',
-  info : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, temporibus?"
+  info : "Buckingham Palace is the London residence and administrative headquarters of the monarch of the United Kingdom. Located in the City of Westminster, the palace is often at the centre of state occasions and royal hospitality. It has been a focal point for the British people at times of national rejoicing and mourning."
  },
  {
   id : 4,
-  city : 'tokyo',
-  img : 'tokyo.jpg',
-  name : 'tokyo sushi tour',
-  info : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, temporibus?"
+  city : 'france',
+  img : 'france.jpg',
+  name : 'Louvre Museum tour',
+  info : "In a stately palace that was once a royal residence, the Louvre ranks among the top European collections of fine arts. Many of Western Civilization's most famous works are found here, including the Mona Lisa by Leonardo DaVinci, the Wedding Feast at Cana by Veronese, and the 1st-century-BC Venus de Milo sculpture.The collection owes its wealth to the contributions of various kings who lived in the Louvre. Other pieces were added as a result of France's treaties with the Vatican and the Republic of Venice, and from the spoils of Napoléon."
  }
 ]
