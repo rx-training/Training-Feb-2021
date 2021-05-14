30567a1b52a274d1533edbd08d3929e717f5e49f
parthshah889900 <parthshah889900@gmail.com>
2021-05-14 12:38:37 +0530

    REFS: #246 and fixes #246(mis file from last day of typescript)

---
+++ Modules/TypeScript/Day4/Parth Shah/Practice/set.ts
@@ -0,0 +1,30 @@
+                                            //==============Set Practices0===========//
+
+
+                                            let studentEntries = new Set();  
+   
+//Add Values  
+studentEntries.add("Parth");  
+studentEntries.add("Shahbhai");  
+studentEntries.add("Mehtabhai");  
+studentEntries.add("Gadabhai");   
+studentEntries.add("Iyerbhai");   
+  
+//Returns Set data  
+console.log(studentEntries);   
+   
+//Check value is present or not  
+console.log(studentEntries.has("Gadabhai"));        
+console.log(studentEntries.has(10));        
+   
+//It returns size of Set  
+console.log(studentEntries.size);    
+   
+//Delete a value from set  
+console.log(studentEntries.delete("Iyerbhai"));      
+   
+//Clear whole Set  
+studentEntries.clear();   
+  
+//Returns Set data after clear method.  
+console.log(studentEntries);  
