1918de7b1f3e82e67fc734fa19fb6343688a1549
Tarun <taroonm345@gmail.com>
2021-05-24 10:51:01 +0530

    REFS #244 and fixes #244

--- Modules/React/Day8-9/TarunMoradiya/Assignment/student/student-reg-node/config/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  publicKey: "public_T4+b4l6DBIKW6sVPH3L+DJv8TkY=",
-  privateKey: "private_iHvKJRh42h8bw7zamDDjareNFJE=",
-  urlEndpoint: "https://ik.imagekit.io/z1hlockgora",
-};
