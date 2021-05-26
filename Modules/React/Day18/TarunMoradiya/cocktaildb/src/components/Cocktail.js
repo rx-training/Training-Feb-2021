c79daefa5922d96ba74af8b1bbf2ec6259a60fe5
Tarun <taroonm345@gmail.com>
2021-05-26 14:18:07 +0530

    REFS #244 and fixes #244

--- Modules/React/Day18/TarunMoradiya/cocktaildb/src/components/Cocktail.js
+++ Modules/React/Day18/TarunMoradiya/cocktaildb/src/components/Cocktail.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function CockTail() {
-  return <div></div>;
+export default function Cocktail({ image, name, id, info, glass }) {
+  return (
+    <article className="cocktail">
+      <div className="img-container">
+        <img src={image} alt={name} />
+      </div>
+      <div className="cocktail-footer">
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className="btn btn-primary btn-details">
+          details
+        </Link>
+      </div>
+    </article>
+  );
 }
--- Modules/React/Day18/TarunMoradiya/cocktaildb/src/pages/About.js
+++ Modules/React/Day18/TarunMoradiya/cocktaildb/src/pages/About.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 export const About = () => {
-  return <div></div>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+        veniam quam optio qui velit distinctio molestiae facilis voluptatum
+        officiis! Voluptas. Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Eaque, pariatur!
+      </p>
+    </section>
+  );
 };
