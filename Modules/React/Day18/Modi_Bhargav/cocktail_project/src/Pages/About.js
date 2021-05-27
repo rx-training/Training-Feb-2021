46723caf16c46048954fc2bbaf96d260ebe34d87
ModiBhargav98 <modibhargav1998@gmail.com>
2021-05-27 18:36:48 +0530

    REFS:#244 and Fixes:#244 (Day-18) CockTail_project

---
+++ Modules/React/Day18/Modi_Bhargav/cocktail_project/src/Pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="srction-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+        debitis, nobis quam dolores repudiandae quidem ut, a possimus dicta
+        maxime distinctio reprehenderit aperiam odio officia asperiores sequi!
+        Ducimus officiis repudiandae, voluptate debitis quasi consequuntur
+        alias, cumque eveniet dignissimos eius vero hic maxime consectetur
+        suscipit quis dolores officia odio. Autem cumque accusantium, sequi
+        atque alias, pariatur sunt ipsum vero quo incidunt architecto nemo.
+      </p>
+    </section>
+  );
+};
+export default About;
