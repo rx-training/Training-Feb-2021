bc383a84c7f7b6700845bf56015be6f1d12cbaee
parthshah889900 <parthshah889900@gmail.com>
2021-06-03 00:11:25 +0530

    REFS :#246 and fixes #246(Day19/20 completed api of this assignement)

---
+++ Modules/Angular/Day18/Parth Shah/Assignment/CrudAngular/CrudAngular/Controllers/StudentsController.cs
@@ -0,0 +1,99 @@
+﻿using CrudAngular.Models.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudAngular.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        IStudent _student;
+
+        public StudentController(IStudent student)
+        {
+            this._student = student;
+        }
+
+        [HttpGet]
+        public IEnumerable<CrudAngular.Models.Student> GetStudents()
+        {
+            return _student.GetAll();
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<CrudAngular.Models.Student> GetStudents(int id)
+        {
+            var student = _student.GetById(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return student;
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<CrudAngular.Models.Student> PutStudents(int id, CrudAngular.Models.Student student)
+        {
+
+            try
+            {
+                _student.Update(student);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return GetStudents(id);
+
+        }
+
+
+        [HttpPost]
+        public ActionResult<CrudAngular.Models.Student> PostStudents(CrudAngular.Models.Student student)
+        {
+
+            try
+            {
+                _student.Create(student);
+            }
+            catch (DbUpdateException)
+            {
+                if (_student.Any(e => e.Id == student.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetStudents", new { id = student.Id }, student);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            var cinema = _student.GetById(id);
+            if (cinema == null)
+            {
+                return NotFound();
+            }
+
+            _student.Delete(cinema);
+
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ Modules/Angular/Day18/Parth Shah/Assignment/CrudAngular/CrudAngular/Models/Repository/GenericRepository.cs
@@ -0,0 +1,74 @@
+﻿using CrudAngular.Models.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudAngular.Models.Repository
+{
+    public abstract class GenericRepository<T> : IGenericInterface<T> where T : class
+    {
+        protected readonly StudContext context;
+
+        public GenericRepository(StudContext _context)
+        {
+            context = _context;
+        }
+
+
+        public bool Any(Func<T, bool> predicate)
+        {
+            return context.Set<T>().Any(predicate);
+        }
+
+        public bool Any()
+        {
+            return context.Set<T>().Any();
+        }
+
+        public bool Any(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count(Func<T, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(T entity)
+        {
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            context.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<T> Find(Func<T, bool> predicate)
+        {
+            return context.Set<T>().Where(predicate);
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return context.Set<T>();
+        }
+
+        public virtual T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+}
+
